9e460129236316c4c31cf1446cebb34f4e234392
blossom14 <wlsdud0581@naver.com>
2016-07-30 20:17:18 +0900

    add avatar into registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
 
-  devise_for :users
   resources :story
 
   resources :between do 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727064232) do
+ActiveRecord::Schema.define(version: 20160730095031) do
 
   create_table "betweens", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20160727064232) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
