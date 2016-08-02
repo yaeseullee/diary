f20617b65de75dcafd03f9f2dfa75fdc6171a812
blossom14 <wlsdud0581@naver.com>
2016-08-02 14:01:10 +0900

    solve comment problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,14 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@between = Between.find(params[:between_id])
-		@comment = @between.comments.create(comment_params)
+		@comment = @between.comments.new(comment_params)
+		@comment.commenter = current_user.email
+		@comment.save
 		redirect_to between_path(@between)
 	end
 
 	private
 		def comment_params
-			params.require(:comment).permit(:commenter, :body)
+			params.require(:comment).permit(:body)
 		end
 end
